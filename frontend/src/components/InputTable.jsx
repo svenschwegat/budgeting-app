3d56d635e48f9dd3e7ca0c2297cbdf0a9356bc32
Sven Schwegat <sven.schwegat+github@gmail.com>
2025-03-14 18:32:26 +0100

    formatted input table

--- frontend/src/components/InputTable.jsx
+++ frontend/src/components/InputTable.jsx
@@ -1,59 +1,72 @@
 'use client'
-import React, { useState } from 'react'
-import { Table, TableHeader, TableBody, TableColumn, TableRow, TableCell, getKeyValue } from "@heroui/react";
+import React from 'react'
+import { DeleteIcon } from '../../public/DeleteIcon';
+import { Tooltip, Table, TableHeader, TableBody, TableColumn, TableRow, TableCell } from "@heroui/react";
 import { Select, SelectItem } from '@heroui/react';
 
 const columns = [
-    { key: "date", label: "Date", editable: false },
-    { key: "name", label: "Name", editable: false },
-    { key: "purpose", label: "Purpose", editable: false },
-    { key: "amount", label: "Amount", editable: false },
-    { key: "category", label: "Category", editable: true },
+  { key: "date", label: "Date" },
+  { key: "name", label: "Name" },
+  { key: "purpose", label: "Purpose" },
+  { key: "amount", label: "Amount" },
+  { key: "category", label: "Category" },
+  { key: "actions", label: "" }
 ];
 
-export const CategorySelector = ({categories, selectedCategory, onChange}) => {
-  return(
-    <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-      <Select className="max-w-xs"  aria-label="Selected category" defaultSelectedKeys={[selectedCategory.toString()]}> 
+const CategorySelector = ({ categories, selectedCategory }) => (
+  <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+    <Select
+      className="max-w-xs"
+      aria-label="Selected category"
+      defaultSelectedKeys={[selectedCategory.toString()]}
+    >
       {categories.map((category) => (
-          <SelectItem key={category.id}>{category.label}</SelectItem>
+        <SelectItem key={category.id}>{category.label}</SelectItem>
       ))}
-      </Select>
-    </div>
-  )
-}
+    </Select>
+  </div>
+);
+
+const ActionButtons = () => (
+  <div className='flex items-center justify-center h-full'>
+    <Tooltip color="danger" content="Delete transaction">
+      <span className="text-lg text-danger cursor-pointer active:opacity-50">
+        <DeleteIcon />
+      </span>
+    </Tooltip>
+  </div>
+);
 
 export default function InputTable({ data, categories }) {
-    const [selectedCategories, setSelectedCategories] = useState({});
-    const handleCategoryChange = (key, value) => {
-        setSelectedCategories((prev) => ({
-            ...prev,
-            [key]: value,
-        }));
-    };
+  const renderCell = React.useCallback((item, columnKey) => {
+    const cellValue = item[columnKey];
 
-    return (
-      <Table removeWrapper aria-label="Input table">
-        <TableHeader columns={columns}>
-          {(column) => <TableColumn width='200' key={column.key}>{column.label}</TableColumn>}
-        </TableHeader>
-        <TableBody items={data}>
+    switch (columnKey) {
+      case "date":
+        const formattedDate = new Date(cellValue).toLocaleDateString('de-DE', {
+          day: '2-digit', month: '2-digit', year: 'numeric' });
+        return formattedDate;
+      case "category":
+        return <CategorySelector categories={categories} selectedCategory={item.category} />;
+      case "actions":
+        return <ActionButtons />;
+      default:
+        return cellValue;
+    }
+  }, []);
+  
+  return (
+    <Table removeWrapper aria-label="Input table">
+      <TableHeader columns={columns}>
+        {(column) => <TableColumn width='200' key={column.key}>{column.label} </TableColumn>}
+      </TableHeader>
+      <TableBody items={data}>
         {(item) => (
-            <TableRow key={item.key}>
-            {(columnKey) => (
-                <TableCell>
-                {columnKey === 'category' ? (
-                    <CategorySelector
-                    categories = {categories}
-                    selectedCategory = {item.category}
-                    onChange={(e) => handleCategoryChange(item.id, e.target.value)}
-                    />) : (getKeyValue(item, columnKey))}
-                </TableCell>
-            )}
-            </TableRow>
+          <TableRow key={item.key}>
+            {(columnKey) => (<TableCell>{renderCell(item, columnKey)}</TableCell>)}
+          </TableRow>
         )}
-        </TableBody>
-      </Table>
-    );
+      </TableBody>
+    </Table>
+  );
 }
-
